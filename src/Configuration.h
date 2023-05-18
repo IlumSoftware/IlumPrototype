427fcc3da377b6e2fc246fe74890efd4d807cf91
arrowdef <franck.bignon@ilumsoftware.dev>
2023-05-18 22:46:22 +0200

    FB : template v0.1

---
+++ src/Configuration.h
@@ -0,0 +1,31 @@
+#ifndef CONFIGURATION_HPP
+#define CONFIGURATION_HPP
+
+#include <SFML/Graphics.hpp> //Texture
+
+#include "ResourceManager.h" //ResourceManager
+#include "ActionMap.h" //ActionMap
+
+class Configuration
+{
+    public:
+        Configuration() = delete;
+        Configuration(const Configuration&) = delete;
+        Configuration& operator=(const Configuration&) = delete;
+
+        enum Textures : int {Player};
+        static ResourceManager<sf::Texture,int> textures;
+
+        enum PlayerInputs : int {Up,Left,Right};
+        static ActionMap<int> playerInputs;
+
+        static void initialize();
+
+    private:
+
+        static void initTextures();
+
+        static void initPlayerInputs();
+};
+#endif
+
---
+++ src/ResourceManager.h
@@ -0,0 +1,43 @@
+#ifndef RESOURCEMANAGER_H
+#define RESOURCEMANAGER_H
+
+#include <unordered_map> //unordered_map
+#include <string> //string
+#include <memory> //unique_ptr
+
+#include <SFML/Audio.hpp> //sf::Music
+
+template<typename RESOURCE,typename IDENTIFIER = int>
+class ResourceManager
+{
+    public:
+        ResourceManager(const ResourceManager&) = delete;
+        ResourceManager& operator=(const ResourceManager&) = delete;
+        ResourceManager() = default;
+        template<typename ... Args>
+        void load(const IDENTIFIER& id,Args&& ... args);
+        RESOURCE& get(const IDENTIFIER& id)const;
+    private:
+        std::unordered_map<IDENTIFIER,std::unique_ptr<RESOURCE>> _map;
+};
+
+template<typename IDENTIFIER>
+class ResourceManager<sf::Music,IDENTIFIER>
+{
+    public:
+        ResourceManager(const ResourceManager&) = delete;
+        ResourceManager& operator=(const ResourceManager&) = delete;
+
+        ResourceManager() = default;
+
+        template<typename ... Args>
+        void load(const IDENTIFIER& id,Args&& ... args);
+
+        sf::Music& get(const IDENTIFIER& id)const;
+
+    private:
+        std::unordered_map<IDENTIFIER,std::unique_ptr<sf::Music>> _map;
+};
+
+#include "ResourceManager.tpl"
+#endif
